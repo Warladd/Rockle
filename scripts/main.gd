a32c497fa3e0152f1729bcdc328d16de9b05defc
Warlordthe3rd <danieljaydenjohnson@gmail.com>
2024-05-04 16:26:22 -0400

    finished prototype!
    Added art, made pressing a button do cool animation, added save system.
    Next I'm going to add randomized sparks when clicking on the rock and sfx.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,9 @@
 extends Node2D
-@export var gear_coins : int = 0
-var gear_amount : int = 1
+var constant_gears : int = 0
 @export_group("Nodes")
 @export var gear_label : Label
+@export var shop_label_1 : Label
+@export var shop_label_2 : Label
 @export var shop : CanvasLayer
 @export var shop_button : Button
 @export var straight_label : Label
@@ -10,95 +11,209 @@ var gear_amount : int = 1
 @export var uppercut_label : Label
 @export var hold_label : Label
 @export var click_button : TextureButton
-
-# Modifiers
-var straight : int = 0
-var kick : int = 0
-var upper : int = 0
-var hold : int = 0
-
-# Structure
-var structure = "disk"
-
-# Cost
-var straight_cost : int = 5
-var kick_cost : int = 100
-var upper_cost : int = 1000
-var hold_cost : int = 25000
-
-var pillar_cost : int = 150
-var boulder_cost : int = 2000
-var wall_cost : int = 30000
+@export var disk_button : Button
+@export var pillar_button : Button
+@export var pillar_label : Label
+@export var boulder_button : Button
+@export var boulder_label : Label
+@export var wall_button : Button
+@export var wall_label : Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	shop.hide()
-	gear_amount = 1
-	if straight > 0:
-		gear_amount += straight
-	if kick > 0:
-		gear_amount += kick * 2
-	if upper > 0:
-		gear_amount += upper * 5
-	if hold > 0:
-		gear_amount += hold * 20
+	SaveSystem.load_game()
+	straight_label.text = str(SaveSystem.save_game.straight_cost) + " " + "Coins"
+	kick_label.text = str(SaveSystem.save_game.kick_cost) + " " + "Coins"
+	uppercut_label.text = str(SaveSystem.save_game.upper_cost) + " " + "Coins"
+	hold_label.text = str(SaveSystem.save_game.hold_cost) + " " + "Coins"
+	if SaveSystem.save_game.pillar:
+		pillar_label.hide()
+	if SaveSystem.save_game.boulder:
+		boulder_label.hide()
+	if SaveSystem.save_game.wall:
+		wall_label.hide()
+	if SaveSystem.save_game.structure == "disk":
+		click_button.texture_normal = load("res://assets/structures/normal/disk.png")
+		click_button.texture_hover = load("res://assets/structures/hover/disk_bigger.png")
+		click_button.texture_pressed = load("res://assets/structures/click/disk_click.png")
+		constant_gears = 0
+	elif SaveSystem.save_game.structure == "pillar":
+		click_button.texture_normal = load("res://assets/structures/normal/pillar.png")
+		click_button.texture_hover = load("res://assets/structures/hover/pillar_bigger.png")
+		click_button.texture_pressed = load("res://assets/structures/click/pillar_click.png")
+		constant_gears = 1
+	elif SaveSystem.save_game.structure == "boulder":
+		click_button.texture_normal = load("res://assets/structures/normal/boulder.png")
+		click_button.texture_hover = load("res://assets/structures/hover/boulder_bigger.png")
+		click_button.texture_pressed = load("res://assets/structures/click/boulder_click.png")
+		constant_gears = 5
+	elif SaveSystem.save_game.structure == "wall":
+		click_button.texture_normal = load("res://assets/structures/normal/wall.png")
+		click_button.texture_hover = load("res://assets/structures/hover/wall_bigger.png")
+		click_button.texture_pressed = load("res://assets/structures/click/wall_click.png")
+		constant_gears = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta) -> void:
-	pass
+	if SaveSystem.save_game.structure == "disk":
+		disk_button.text = "Equipped"
+		disk_button.add_theme_color_override("Font Color", Color("2e2e2e"))
+		if SaveSystem.save_game.pillar:
+			pillar_button.text = "Use"
+		if SaveSystem.save_game.boulder:
+			boulder_button.text = "Use"
+		if SaveSystem.save_game.wall:
+			wall_button.text = "Use"
+	elif SaveSystem.save_game.structure == "pillar":
+		pillar_button.text = "Equipped"
+		pillar_button.add_theme_color_override("Font Color", Color("2e2e2e"))
+		disk_button.text = "Use"
+		if SaveSystem.save_game.boulder:
+			boulder_button.text = "Use"
+		if SaveSystem.save_game.wall:
+			wall_button.text = "Use"
+	elif SaveSystem.save_game.structure == "boulder":
+		boulder_button.text = "Equipped"
+		boulder_button.add_theme_color_override("Font Color", Color("2e2e2e"))
+		if SaveSystem.save_game.pillar:
+			pillar_button.text = "Use"
+		disk_button.text = "Use"
+		if SaveSystem.save_game.wall:
+			wall_button.text = "Use"
+	elif SaveSystem.save_game.structure == "wall":
+		wall_button.text = "Equipped"
+		wall_button.add_theme_color_override("Font Color", Color("2e2e2e"))
+		if SaveSystem.save_game.pillar:
+			pillar_button.text = "Use"
+		if SaveSystem.save_game.boulder:
+			boulder_button.text = "Use"
+		disk_button.text = "Use"
+
+func _input(event) -> void:
+	if event.is_action_pressed("esc"):
+		get_tree().quit(0)
 
 func _on_texture_button_pressed() -> void:
-	gear_coins += gear_amount
-	gear_label.text = "Gear Coins: " + str(gear_coins)
+	SaveSystem.save_game.gear_coins += SaveSystem.save_game.gear_amount
+	gear_label.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
 
 func _on_button_pressed() -> void:
 	shop.show()
+	shop_label_1.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+	shop_label_2.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
 
-func _on_straight_button_pressed():
-	if gear_coins >= straight_cost:
-		gear_coins -= straight_cost
-		straight_cost *= 2
-		straight_cost = roundi(straight_cost)
-		straight_label.text = str(straight_cost) + " " + "Coins"
-		gear_amount += 1
-		straight += 1
-		gear_label.text = "Gear Coins: " + str(gear_coins)
+func _on_straight_button_pressed() -> void:
+	if SaveSystem.save_game.gear_coins >= SaveSystem.save_game.straight_cost:
+		SaveSystem.save_game.gear_coins -= SaveSystem.save_game.straight_cost
+		SaveSystem.save_game.straight_cost *= 2
+		SaveSystem.save_game.straight_cost = roundi(SaveSystem.save_game.straight_cost)
+		straight_label.text = str(SaveSystem.save_game.straight_cost) + " " + "Coins"
+		SaveSystem.save_game.gear_amount += 1
+		gear_label.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		shop_label_1.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		shop_label_2.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		SaveSystem.saving()
 
-func _on_kick_button_pressed():
-	if gear_coins >= kick_cost:
-		gear_coins -= kick_cost
-		kick_cost *= 2.5
-		kick_cost = roundi(kick_cost)
-		kick_label.text = str(kick_cost) + " " + "Coins"
-		gear_amount += 5
-		kick += 1
-		gear_label.text = "Gear Coins: " + str(gear_coins)
+func _on_kick_button_pressed() -> void:
+	if SaveSystem.save_game.gear_coins >= SaveSystem.save_game.kick_cost:
+		SaveSystem.save_game.gear_coins -= SaveSystem.save_game.kick_cost
+		SaveSystem.save_game.kick_cost *= 2.5
+		SaveSystem.save_game.kick_cost = roundi(SaveSystem.save_game.kick_cost)
+		kick_label.text = str(SaveSystem.save_game.kick_cost) + " " + "Coins"
+		SaveSystem.save_game.gear_amount += 5
+		gear_label.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		shop_label_1.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		shop_label_2.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		SaveSystem.saving()
 
-func _on_uppercut_button_pressed():
-	if gear_coins >= upper_cost:
-		gear_coins -= upper_cost
-		upper_cost *= 2
-		upper_cost = roundi(upper_cost)
-		uppercut_label.text = str(upper_cost) + " " + "Coins"
-		gear_amount += 50
-		upper += 1
-		gear_label.text = "Gear Coins: " + str(gear_coins)
+func _on_uppercut_button_pressed() -> void:
+	if SaveSystem.save_game.gear_coins >= SaveSystem.save_game.upper_cost:
+		SaveSystem.save_game.gear_coins -= SaveSystem.save_game.upper_cost
+		SaveSystem.save_game.upper_cost *= 2
+		SaveSystem.save_game.upper_cost = roundi(SaveSystem.save_game.upper_cost)
+		uppercut_label.text = str(SaveSystem.save_game.upper_cost) + " " + "Coins"
+		SaveSystem.save_game.gear_amount += 50
+		gear_label.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		shop_label_1.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		shop_label_2.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		SaveSystem.saving()
 
-func _on_hold_button_pressed():
-	if gear_coins >= hold_cost:
-		gear_coins -= hold_cost
-		hold_cost *= 1.5
-		hold_cost = roundi(hold_cost)
-		hold_label.text = str(hold_cost) + " " + "Coins"
-		gear_amount += 1200
-		hold += 1
-		gear_label.text = "Gear Coins: " + str(gear_coins)
+func _on_hold_button_pressed() -> void:
+	if SaveSystem.save_game.gear_coins >= SaveSystem.save_game.hold_cost:
+		SaveSystem.save_game.gear_coins -= SaveSystem.save_game.hold_cost
+		SaveSystem.save_game.hold_cost *= 1.5
+		SaveSystem.save_game.hold_cost = roundi(SaveSystem.save_game.hold_cost)
+		hold_label.text = str(SaveSystem.save_game.hold_cost) + " " + "Coins"
+		SaveSystem.save_game.gear_amount += 1200
+		gear_label.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		shop_label_1.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		shop_label_2.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+		SaveSystem.saving()
 
 func _on_exit_shop_button_pressed():
 	shop.visible = false
 
 func _on_pillart_button_pressed():
-	if gear_coins >= pillar_cost:
-		gear_coins -= pillar_cost
-		click_button.texture_normal = "res://assets/structures/pillar.png"
-		click_button.texture_hover = "res://assets/structures/pillar_bigger.png"
+	if SaveSystem.save_game.gear_coins >= SaveSystem.save_game.pillar_cost or SaveSystem.save_game.pillar:
+		if !SaveSystem.save_game.pillar:
+			SaveSystem.save_game.gear_coins -= SaveSystem.save_game.pillar_cost
+			SaveSystem.save_game.pillar = true
+			shop_label_1.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+			shop_label_2.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+			pillar_label.hide()
+		click_button.texture_normal = load("res://assets/structures/normal/pillar.png")
+		click_button.texture_hover = load("res://assets/structures/hover/pillar_bigger.png")
+		click_button.texture_pressed = load("res://assets/structures/click/pillar_click.png")
+		constant_gears = 1
+		SaveSystem.save_game.structure = "pillar"
+		SaveSystem.saving()
+
+func _on_boulder_button_pressed() -> void:
+	if SaveSystem.save_game.gear_coins >= SaveSystem.save_game.boulder_cost or SaveSystem.save_game.boulder:
+		if !SaveSystem.save_game.boulder:
+			SaveSystem.save_game.gear_coins -= SaveSystem.save_game.boulder_cost
+			SaveSystem.save_game.boulder = true
+			shop_label_1.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+			shop_label_2.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+			boulder_label.hide()
+		click_button.texture_normal = load("res://assets/structures/normal/boulder.png")
+		click_button.texture_hover = load("res://assets/structures/hover/boulder_bigger.png")
+		click_button.texture_pressed = load("res://assets/structures/click/boulder_click.png")
+		constant_gears = 5
+		SaveSystem.save_game.structure = "boulder"
+		SaveSystem.saving()
+
+func _on_wall_button_pressed() -> void:
+	if SaveSystem.save_game.gear_coins >= SaveSystem.save_game.wall_cost or SaveSystem.save_game.wall:
+		if !SaveSystem.save_game.wall:
+			SaveSystem.save_game.gear_coins -= SaveSystem.save_game.wall_cost
+			SaveSystem.save_game.wall = true
+			shop_label_1.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+			shop_label_2.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+			wall_label.hide()
+			SaveSystem.saving()
+		click_button.texture_normal = load("res://assets/structures/normal/wall.png")
+		click_button.texture_hover = load("res://assets/structures/hover/wall_bigger.png")
+		click_button.texture_pressed = load("res://assets/structures/click/wall_click.png")
+		constant_gears = 100
+		SaveSystem.save_game.structure = "wall"
+		SaveSystem.saving()
+
+func _on_diskartonline_button_pressed() -> void:
+	click_button.texture_normal = load("res://assets/structures/normal/disk.png")
+	click_button.texture_hover = load("res://assets/structures/hover/disk_bigger.png")
+	click_button.texture_pressed = load("res://assets/structures/click/disk_click.png")
+	constant_gears = 0
+	SaveSystem.save_game.structure = "disk"
+
+
+func _on_structure_timer_timeout():
+	SaveSystem.save_game.gear_coins += constant_gears
+	gear_label.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+	shop_label_1.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+	shop_label_2.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
+
+
+func _on_save_timer_timeout():
+	SaveSystem.saving()
---
+++ scripts/variables.gd
@@ -0,0 +1,21 @@
+extends Resource
+class_name SaveFile
+
+var gear_coins : int = 0
+var gear_amount : int = 1
+
+# Structures
+var structure = "disk"
+var pillar : bool = false
+var boulder : bool = false
+var wall : bool = false
+
+# Cost
+var straight_cost : int = 50
+var kick_cost : int = 1000
+var upper_cost : int = 25000
+var hold_cost : int = 1000000
+
+var pillar_cost : int = 150
+var boulder_cost : int = 2000
+var wall_cost : int = 30000
