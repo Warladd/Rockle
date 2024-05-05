49d92813f826485d6237aa13aeb1b4cc7fdd6625
Warlordthe3rd <danieljaydenjohnson@gmail.com>
2024-05-05 08:01:35 -0400

    adding pillar

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,7 +5,6 @@ extends Node2D
 @export var shop_label_2 : Label
 @export var shop : CanvasLayer
 @export var shop_button : Button
-@export var pillar_label : Label
 @export var boulder_label : Label
 @export var wall_label : Label
 @export var player_sprite : AnimatedSprite2D
@@ -16,11 +15,20 @@ extends Node2D
 @export var disk_sprite : Sprite2D
 var disk_ready : bool = true
 var disk_tween : Tween 
-
 var disk_time = 0.1
+
+# Pillar
+@export var pillar_sprite : Sprite2D
+@export var pillar_label : Label
+var pillar_tween : Tween 
+var pillar_ready : bool = true
 var pillar_time = 5
+
+# Boulder
 var boulder_time = 30
+var boulder_ready : bool = true
 var wall_time = 90
+var wall_ready : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -40,12 +48,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta) -> void:
-	print("can disk: ", disk_ready)
+	if !disk_ready and pillar_ready:
+		pillar_tween = create_tween()
+		pillar_tween.tween_property(pillar_sprite, "position", Vector2(428, 313), 0.5)
+		await pillar_tween.finished
 
 func _input(event) -> void:
 	if event.is_action_pressed("l_click"):
 		if disk_ready:
-			print("disking")
 			disk_ready = false
 			disk_timer.start()
 			disk_tween = create_tween()
@@ -53,8 +63,6 @@ func _input(event) -> void:
 			gear_label.text = "Gear Coins: " + str(SaveSystem.save_game.gear_coins)
 			player_sprite.play("straight")
 			disk_tween.tween_property(disk_sprite, "position", Vector2(800, 313), 0.1)
-			await disk_tween.finished
-			player_sprite.play("default")
 	if event.is_action_pressed("esc"):
 		SaveSystem.saving()
 		get_tree().quit(0)
@@ -67,7 +75,7 @@ func _on_button_pressed() -> void:
 func _on_straight_button_pressed() -> void:
 	if SaveSystem.save_game.gear_coins >= SaveSystem.save_game.disk_cost:
 		SaveSystem.save_game.gear_coins -= SaveSystem.save_game.disk_cost
-		SaveSystem.save_game.disk_cost *= 2
+		SaveSystem.save_game.disk_cost = (SaveSystem.save_game.disk_cost + 5) * 1.2
 		SaveSystem.save_game.disk_cost = roundi(SaveSystem.save_game.disk_cost)
 		disk_label.text = str(SaveSystem.save_game.disk_cost) + " " + "Coins"
 		SaveSystem.save_game.disk += 1
@@ -119,9 +127,10 @@ func _on_save_timer_timeout():
 	SaveSystem.saving()
 
 func _on_disk_timer_timeout():
-	print("can disk again")
+	player_sprite.play("default")
 	disk_ready = true
-	disk_sprite.position = Vector2(464, 413)
+	disk_sprite.position = Vector2(430, 413)
+	if 
 	disk_tween = create_tween()
-	disk_tween.tween_property(disk_sprite, "position", Vector2(464, 313), 0.1)
+	disk_tween.tween_property(disk_sprite, "position", Vector2(440, 313), 0.1)
 	await disk_tween.finished
