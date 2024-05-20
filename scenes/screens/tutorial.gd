a87c7df5a1548785d3713464fe83904af0ecaaf0
Warladd <danieljaydenjohnson@gmail.com>
2024-05-20 19:46:24 -0400

    started implementing tutorial

---
+++ scenes/screens/tutorial.gd
@@ -0,0 +1,15 @@
+extends Node2D
+@export var dialogue_box : Control
+@export var big_v_small : AnimatedSprite2D
+@export var move_v_no_move : AnimatedSprite2D
+@export var mulitpliers : Sprite2D
+@export var structure_damage_values : Sprite2D
+@export var grounded_v_ungrounded : Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
